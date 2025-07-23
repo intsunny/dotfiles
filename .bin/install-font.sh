e512499cfef3a71825bf94323c8b888b1a62c20a
Sunny Dubey <sunny@audiowiki.org>
2025-07-23 12:18:03 +0200

    Updates

--- .bin/install-font.sh
+++ .bin/install-font.sh
@@ -1,15 +1,39 @@
 #!/bin/sh
 
-try_exec() {
+run() {
     ${@}
 
-    if [ "$?" == 0 ] ; then
-        echo "OK!"
-    else
+    if [ "$?" != 0 ] ; then
+        echo "We had an issue with the command: ${@}"
         echo "ERR0R! Abort!"
         exit 1
     fi
 }
+
+run_with_echo() {
+    ${@} 1>/dev/null
+
+    if [ "$?" == 0 ] ; then
+        echo "OK!"
+    else
+        echo "We had an issue with the command: ${@}"
+        echo "ERR0R! Abort!"
+        exit 1
+    fi
+}
+
+if [ -d extract ] ; then
+    echo "extract directory not empty!"
+    echo "Plz manually clear and rerun script"
+    exit 1
+fi
+
+if [ "$1" == "" ] ; then
+    echo "Plz provide URL to grab fonts front. (This script is designed to work with the fronts provided by:"
+    echo "https://www.nerdfonts.com/"
+    
+    exit 1
+fi
 
 if [ -d ~/.local/share/fonts ] ; then
     echo -n "~/.local/share/fonts exists. Care to make a backup? [Y/n]: "
@@ -19,7 +43,7 @@ if [ -d ~/.local/share/fonts ] ; then
     if [ "${BACKUP_ANS}" != 'n' ] ; then
         DATE=$(date +"%Y-%m-%d_%H-%M-%S")
 
-        try_exec tar cvf ~/.local/share/fonts-${DATE}.tar -C ~/.local/share fonts
+        run_with_echo tar cvf ~/.local/share/fonts-${DATE}.tar -C ~/.local/share fonts
     fi
 else
     echo -n "The direction ~/.local/share/fonts/ does not exist. Create? [y/N]: "
@@ -28,6 +52,27 @@ else
 
     if [ "${CREATE_ANS}" == 'y' ] ; then
         echo -n "Making ~/.local/share/fonts ...."
-        try_exec mkdir -p ~/.local/share/fonts
+        run_with_echo mkdir -p ~/.local/share/fonts
     fi
 fi
+
+run mkdir extract
+
+echo "Downloading font ..."
+
+run_with_echo curl -LO "$1" --output-dir extract
+
+echo "Extracting font ..."
+
+run_with_echo unzip extract/*.zip -d extract
+
+echo "Copying font files ..."
+
+run_with_echo cp extract/*.ttf ~/.local/share/fonts
+
+echo "Regenerating font cache"
+
+run_with_echo fc-cache -f
+
+echo "You can look for your new font via: "
+echo "fc-list"
